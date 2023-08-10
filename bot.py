ff7d0e0d1bde9a96b8eaf8ec1fe11ca23aee06c3
Shivasish10 <141301849+Shivasish10@users.noreply.github.com>
2023-08-10 10:50:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,37 @@
-import slack
 import os
-from pathlib import Path
-from dotenv import load_dotenv
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+import json
+import requests
 
-
-env_path = Path('.')/'.env'
-
-load_dotenv(dotenv_path=env_path)
-
-app = Flask(__name__)
-## Sending SIGNING SECREAT , '/slack/events', app(webserver) to web server
-slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'],'/slack/events',app) 
-
-client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-
-## chat_postmessage is used here to post text messsage in yhe channel that we have created 
-client.chat_postMessage(channel='#new-bot',text = 'Hello World') 
-
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event',{})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    client.chat_postMessage(channel=channel_id,text = text) 
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def lambda_handler(event, context):
+    # Extract user's message from the event
+    user_message = event.get('text', '').lower()
+    
+    # Check if the user's message contains "hi"
+    if 'hi' in user_message:
+        response = "Hi, welcome to the chat bot!"
+    else:
+        response = "Hello, how can I assist you?"
+    
+    # Construct the response payload
+    response_payload = {
+        "text": response
+    }
+    
+    # Get the Slack webhook URL from the environment variable
+    webhook_url = os.environ['SLACK_WEBHOOK_URL']
+    
+    # Send the response payload to Slack using the webhook URL
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(webhook_url, data=json.dumps(response_payload), headers=headers)
+    
+    # Check the response status and return a success message
+    if response.status_code == 200:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Message sent successfully to Slack')
+        }
+    else:
+        return {
+            'statusCode': response.status_code,
+            'body': json.dumps('Failed to send message to Slack')
+        }
